f73bc3f59112701bdd2a85e0dbfefb86d0edad32
frankdsun <hello@world.ca>
2014-11-09 23:47:53 -0500

    model

--- js/router.js
+++ js/router.js
@@ -5,11 +5,11 @@ Todo.Router.map(function (){
 Todo.TodolistRoute = Ember.Route.extend({
 
 	model: function(){
-	 	this.store.push('tododata', {
-	      id: 10,
-	      desc: "Fewer Moving Parts",
-	      isCompleted: false
-	    });
+	 	// this.store.push('tododata', {
+	  //     id: 10,
+	  //     desc: "Fewer Moving Parts",
+	  //     isCompleted: false
+	  //   });
 	 return this.store.find('tododata');
 	},
 
