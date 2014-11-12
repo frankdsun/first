28fe90069a893ce707a2ce33107411202835a609
frankdsun <hello@world.ca>
2014-11-11 23:28:45 -0500

    done

--- js/controller.js
+++ js/controller.js
@@ -51,6 +51,35 @@ Todo.TodolistController = Ember.ArrayController.extend({
 			});
 			this.set('newDesc', '');
 			item.save();
+		},
+		clearCompleted: function(){
+			var items = this.filterBy('isCompleted', true);
+			items.forEach(function(item){
+				item.destroyRecord();
+			});
 		}
-	}
+	},
+	remaining: function(){
+		return this.filterBy('isCompleted', false).get('length');
+	}.property('@each.isCompleted'),
+	completed: function(){
+		return this.filterBy('isCompleted', true).get('length');
+	}.property('@each.isCompleted'),
+	todos: function(){
+		return this.get('remaining') > 1? 'todos' : 'todo';
+	}.property('remaining'),
+	hasCompleted: function(){
+		return !!this.get('completed');
+	}.property('completed'),
+
+	toggleAll: function(key, value){
+		if(value === undefined){
+			return !!this.get('length') && this.isEvery('isCompleted', true);
+		}
+		else {
+			this.setEach('isCompleted', value);
+			this.invoke('save');
+			return value;
+		}
+	}.property('@each.isCompleted')
 });
